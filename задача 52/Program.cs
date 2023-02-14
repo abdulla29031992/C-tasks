bf50e98598160699edb8e4e7f3efdd226cfc24c1
unknown <mr.ad1992@mail.ru>
2023-02-14 11:41:27 +0300

    добавил задачу 52

---
+++ задача 52/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 42: Напишите программу, которая будет преобразовывать десятичное число в двоичное.
+// 45 -> 101101
+// 3 -> 11
+// 2 -> 10
+
+Console.Clear();
+
+string ConvertDecToBin(int number)
+{
+string result = String.Empty;
+while (number != 0)
+{
+result = (number % 2).ToString() + result;
+number /= 2;
+}
+return result;
+}
+
+Console.Write("Введите число: ");
+int n = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine(ConvertDecToBin(n));
