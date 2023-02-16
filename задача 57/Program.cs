40dfc5d2223321fcd5f3ea0205358f560433d3a9
abdulla29031992 <123041242+abdulla29031992@users.noreply.github.com>
2023-02-16 21:13:23 +0300

    Update Program.cs

--- задача 57/Program.cs
+++ задача 57/Program.cs
@@ -4,3 +4,37 @@
 // 0 1 2 3
 // 1 2 3 4
 // 2 3 4 5
+
+Console.Clear();
+int[,] GetArray(int row, int col)
+{
+  int[,] array = new int[row, col];
+  for (int i = 0; i < row; i++)
+  {
+    for (int j = 0; j < col; j++)
+    {
+      array[i, j] = i + j;
+    }
+  }
+  return array;
+}
+
+void PrintArray(int[,] inArray)
+{
+  for (int i = 0; i < inArray.GetLength(0); i++)
+  {
+    for (int j = 0; j < inArray.GetLength(1); j++)
+    {
+      Console.Write($"{inArray[i, j]}\t ");
+    }
+    Console.WriteLine();
+  }
+}
+
+Console.Write("Введите размер строк: ");
+int row = int.Parse(Console.ReadLine()!);
+Console.Write("Введите размер столбцов: ");
+int col = int.Parse(Console.ReadLine()!);
+
+int[,] array = GetArray(row, col);
+PrintArray(array);
